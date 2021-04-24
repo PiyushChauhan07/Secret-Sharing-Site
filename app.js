c82eb5012835e5d8b3e0585a3b104194ec0eb694
PiyushChauhan07 <piyushchouhan240701@gmail.com>
2021-04-24 12:13:38 +0530

    Update encrytion to hashing using salting.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.static("public"));
@@ -17,7 +19,7 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
 const User = new mongoose.model("User",userSchema);
 
@@ -32,16 +34,19 @@ app.get("/login",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }else{
-      console.log(err);
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }else{
+        console.log(err);
+      }
+    });
   });
 });
 
@@ -52,11 +57,13 @@ app.post("/login",function(req,res){
     if(err){
       console.log(err);
     }else{
-      if(foundUser.password === password){
-        res.render("secrets");
-      }else{
-        res.send("Couldn't find the user.Please verify your details.");
-      }
+      bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+          if(result === true){
+            res.render("secrets");
+          }else{
+            res.send("Couldn't find the user.Please verify your details.");
+          }
+      });
     }
   });
 });
