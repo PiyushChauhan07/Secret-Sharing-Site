5d933995516a272f8b442096187c882d9d7f1aac
PiyushChauhan07 <piyushchouhan240701@gmail.com>
2021-05-20 18:01:01 +0530

    Edit redirecting routes

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets", ///where to redirect after google authenticates a user
+    callbackURL: "https://sharewithall.herokuapp.com//auth/google/secrets", ///where to redirect after google authenticates a user
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -78,7 +78,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    callbackURL: "https://sharewithall.herokuapp.com//auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
